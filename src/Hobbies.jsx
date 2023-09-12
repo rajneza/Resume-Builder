a1a5be40c929015bd0339da85c6e85175392dd5e
Ankita7520 <123752695+Ankita7520@users.noreply.github.com>
2023-09-12 16:35:27 +0530

    nation

--- src/Hobbies.jsx
+++ src/Hobbies.jsx
@@ -780,7 +780,7 @@ function Hobbies (){
                   </div>
                 </div>
                 <div className="photo">
-                  <label htmlFor="">Nationality</label> <br />
+                  <label htmlFor="">Nation</label> <br />
                   <input type="text" className="work" value={nation} onChange={handlechange10}/>
                 </div>
 
