a6aba6264fe3ed128e3ae09e55d396b8d2d415e8
SaiSwain <132897931+SaiSwain@users.noreply.github.com>
2023-09-19 00:41:23 +0530

    landing page

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,15 +1,28 @@
 
 import React from "react";
 import "./Navbar.css";
+import sai from "./images/sai.avif";
+import build from "./images/build.png";
+import Image from './images/visual-0c7080adf17f1f207276f613447c924f667dab34b7ac415cd7ef653172defd0b.svg';
+import feature from "./images/feature-1-edf4481d69166ac81917d1e40e6597c8d61aa970ad44367ce78049bf830fbda5.svg";
+import feature1 from "./images/feature-2-a7a471bd973c02a55d1b3f8aff578cd3c9a4c5ac4fc74423d94ecc04aef3492b.svg";
+import feature2 from "./images/feature-2-a7a471bd973c02a55d1b3f8aff578cd3c9a4c5ac4fc74423d94ecc04aef3492b.svg";
+import ankita from "./images/ankita.avif";
+import usha from "./images/usha.avif";
+import rajesh from "./images/rajesh.avif";
     function Navbar() {
   return (
-    <div>
+    <div className="landing-page">
+      
     <nav className="navbar bg-white">
+ 
+    
     <div className="container">
+      
       <div className="navbar-content">
         <div className="brand-and-toggler">
-          <a href="index.html" className="navbar-brand">
-            <img src="assets/images/curriculum-vitae.png" alt="" className="navbar-brand-icon" />
+        <a href="index.html" className="navbar-brand">
+            <img src={build} alt="" className="navbar-brand-icon" />
             <span className="navbar-brand-text">build <span>resume.</span></span>
           </a>
           <button type="button" className="navbar-toggler-btn">
@@ -22,7 +35,7 @@ import "./Navbar.css";
         </div>
       </div>
     </div>
-  </nav>
+   </nav>
    <header className="header bg-bright" id="header">
    <div className="container">
      <div className="header-content text-center">
@@ -30,15 +43,25 @@ import "./Navbar.css";
        <h1 className="lg-title">Only 2% of resumes make it past the first round. Be in the top 2%</h1>
        <p className="text-dark fs-18">Use professional field-tested resume templates that follow the exact 'resume rules' employers look for. Easy to use and done within minutes - try now for free!</p>
        <a href="resume.html" className="btn btn-primary text-uppercase">create my resume</a>
-       <img src="assets/images/sai1.avif" alt="sai" />
      </div>
+     
    </div>
+   <div className="immages">
+        <div className="photo">
+       <img src={sai} alt="sai" /></div>
+       <div className="photo">
+       <img src={usha} alt="sai" /></div>
+       <div className="photo">
+       <img src={ankita} alt="sai" /></div>
+       <div className="photo">
+       <img src={rajesh} alt="sai" /></div>
+     </div>
  </header>
   <div className="section-one">
   <div className="container">
     <div className="section-one-content">
       <div className="section-one-l">
-        <img src="assets/images/sai.avif" alt="" />
+        <img src={Image}/>
       </div>
       <div className="section-one-r text-center">
         <h2 className="lg-title">Use the best resume maker as your guide!</h2>
@@ -57,7 +80,7 @@ import "./Navbar.css";
      <div className="section-items">
        <div className="section-item">
          <div className="section-item-icon">
-           <img src="assets/images/feature-1-edf4481d69166ac81917d1e40e6597c8d61aa970ad44367ce78049bf830fbda5.svg" alt="" />
+           <img src={feature}/>
          </div>
          <h5 className="section-item-title">Make a resume that wins interviews!</h5>
          <p className="text">Use our resume maker with its advanced creation tools to tell a professional story that engages recruiters, hiring managers, and even CEOs.</p>
@@ -65,7 +88,7 @@ import "./Navbar.css";
 
        <div className="section-item">
          <div className="section-item-icon">
-           <img src="assets/images/feature-2-a7a471bd973c02a55d1b3f8aff578cd3c9a4c5ac4fc74423d94ecc04aef3492b.svg" alt="" />
+           <img src={feature1} />
          </div>
          <h5 className="section-item-title">Resume writing made easy!</h5>
          <p className="text">Resume writing has never been this effortless. Pre-generated text, visual designs, and more - all already integrated into the resume maker. Just fill in your details.</p>
@@ -73,7 +96,7 @@ import "./Navbar.css";
 
        <div className="section-item">
          <div className="section-item-icon">
-           <img src="assets/images/feature-3-4e87a82f83e260488c36f8105e26f439fdc3ee5009372bb5e12d9421f32eabdd.svg" alt="" />
+           <img src={feature2} />
          </div>
          <h5 className="section-item-title">A recruiter-tested CV maker tool</h5>
          <p className="text">Our resume builder and its pre-generated content are tested by recruiters and IT experts. We help your CV become truly competitive in the hiring process.</p>
